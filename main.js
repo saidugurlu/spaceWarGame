8a3553f2b192e9250ab352de41184803e8378aa4
saidugurlu <ugurlumsaid@gmx.de>
2022-03-15 16:20:58 +0100

    Added star generator function for background

--- main.js
+++ main.js
@@ -10,5 +10,20 @@ let shipExplosion = new Audio("./sounds/ship-explosion.wav");
 // game variables
 let move;
 let score = 0;
-let x,y
+let x, y;
 let velocity = 1.01;
+
+// create stars for backround
+for (let a = 1; a <= 1000; a++) {
+  const star = document.createElement("div");
+  let x = Math.floor(Math.random() * window.innerWidth);
+  let y = Math.floor(Math.random() * window.innerHeight);
+  star.style.top = y + "px";
+  star.style.left = x + "px";
+  star.classList.add("stars");
+  let starSize = Math.floor(Math.random() * 3);
+  star.style.width = starSize + "px";
+  star.style.height = starSize + "px";
+  star.style.background = starSize < 4 ? "lightgray" : "white";
+  gameArea.appendChild(star);
+}
